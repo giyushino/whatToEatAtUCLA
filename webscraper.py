b3d9a4a53b5b9f49d881d2b080738a6a16a5d189
giyushino <allanzhang440@gmail.com>
2025-01-05 18:52:08 -0800

    update

--- webscraper.py
+++ webscraper.py
@@ -1,13 +1,25 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import requests
 
+
 diningHalls = ["DeNeve", "Epicuria", "BruinPlate", "Covel", "Feast", "Rendezvous", "TheStudy"]
+time = ["Breakfast", "Lunch", "Dinner"]
+diningHallLink = "https://menu.dining.ucla.edu/Menus/{diningHall}/{time}"
+links = []
 
 
-r = requests.get("https://menu.dining.ucla.edu/Menus/Epicuria/Dinner")
-print(r.status_code)
+for hall in diningHalls:
+    for meal in time:
+        print(diningHallLink.format(diningHall = hall, time = meal))
+        r = requests.get(diningHallLink.format(diningHall = hall, time = meal))
+        links.append(r)
 
-soup = BeautifulSoup(r.content, "html.parser")
+print(len(links))
 
-print("".join(char for char in soup.prettify() if ord(char) < 128))
+
+#r = requests.get("https://menu.dining.ucla.edu/Menus/Epicuria/Dinner")
+#print(r.status_code)
+
+#soup = BeautifulSoup(r.content, "html.parser")
+
+#print("".join(char for char in soup.prettify() if ord(char) < 128))
