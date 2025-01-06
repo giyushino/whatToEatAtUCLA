7fb7144b1eebe7ac82b5726f5a31a12021f2718d
giyushino <allanzhang440@gmail.com>
2025-01-05 20:09:50 -0800

    update

--- webscraper.py
+++ webscraper.py
@@ -2,24 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 
 
-diningHalls = ["DeNeve", "Epicuria", "BruinPlate", "Covel", "Feast", "Rendezvous", "TheStudy"]
-time = ["Breakfast", "Lunch", "Dinner"]
-diningHallLink = "https://menu.dining.ucla.edu/Menus/{diningHall}/{time}"
+#diningHalls = ["DeNeve", "Epicuria", "BruinPlate", "HedrickStudy", "FeastAtRieber", "EpicAtAckerman", "TheStudy"]
+diningHalls = ["DeNeve", "Epicuria", "BruinPlate", "FeastAtRieber"]
+
+# Don't forget to remove tomorrow from link in future
+diningHallLink = "https://menu.dining.ucla.edu/Menus/{diningHall}/tomorrow"
 links = []
 
-
 for hall in diningHalls:
-    for meal in time:
-        print(diningHallLink.format(diningHall = hall, time = meal))
-        r = requests.get(diningHallLink.format(diningHall = hall, time = meal))
-        links.append(r)
+    if hall != "FeastAtRieber":
+        r = requests.get(diningHallLink.format(diningHall = hall))
+        links.append([r, hall])
+print(links)
 
-print(len(links))
+"""
+for link in links:
+    """
+    #f = open(r"C:\Users\allan\nvim\python\whatToEatAtUCLA\html\{}.txt".format(link[1]), "w")
+"""
+    soup = BeautifulSoup(link[0].content, "html.parser")
+    f.write("".join(char for char in soup.prettify() if ord(char) < 128))
+    f.close()
+    print(f"Finished writing {link[1]}")
+"""
 
 
-#r = requests.get("https://menu.dining.ucla.edu/Menus/Epicuria/Dinner")
-#print(r.status_code)
 
-#soup = BeautifulSoup(r.content, "html.parser")
 
-#print("".join(char for char in soup.prettify() if ord(char) < 128))
+
+
+
